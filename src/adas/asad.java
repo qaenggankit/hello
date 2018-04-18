d5331f09db8a1437750d7c8be2e7ec77109936a1
qaenggankit <Windows10@NDRT38-ANKIT.ladss11.com>
2018-04-18 17:58:00 +0530

    new entry

---
+++ src/adas/asad.java
@@ -0,0 +1,11 @@
+package adas;
+
+public class asad {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("wdefcasx");
+	}
+
+}
---
+++ src/adas/wefr.java
@@ -0,0 +1,11 @@
+package adas;
+
+public class wefr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("jbfd");
+	}
+
+}
